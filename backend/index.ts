1e77b06fe4907dc15e6ef6e96c37a2302bf10337
Marcus Haaland <marcuhaa@stud.ntnu.no>
2021-10-15 11:19:44 +0200

    feat: connect to mongodb in vm #12

--- backend/index.ts
+++ backend/index.ts
@@ -1,18 +1,23 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 
+// Secrets should be kept secret, for example env file, but hardcoded here for the project
+const DBUSER = 'admin';
+const DBPASSWORD = 'admin';
+const DBNAME = 'pokedex';
+
 const start = async () => {
   const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   };
 
-  const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.rljgw.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+  const uri = `mongodb://${DBUSER}:${DBPASSWORD}@it2810-15.idi.ntnu.no:27017/${DBNAME}?authSource=${DBUSER}`;
 
   try {
     // @ts-ignore https://stackoverflow.com/questions/68806347/argument-of-type-usenewurlparser-boolean-useunifiedtopology-boolean-is-n
     await mongoose.connect(uri, options);
-    console.log('Connected to mongodb');
+    console.log(`Connected to ${DBNAME}`);
   } catch (err) {
     console.error(err);
   }
