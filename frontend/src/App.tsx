08aa365501745f3949bfb0bd1217b5032fe12445
Marcus Haaland <marcuhaa@stud.ntnu.no>
2021-10-16 19:44:03 +0200

    feat: form connects to backend fetching results #1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,24 @@
-function App() {
+import { ChakraProvider, Box } from '@chakra-ui/react';
+import Home from './pages/Home';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const App = () => {
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache(),
+  });
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <ChakraProvider>
+        <ApolloProvider client={client}>
+          {/* TODO: Nav should contain navigation */}
+          <Box p={2}>Nav</Box>
+          <Home />
+        </ApolloProvider>
+      </ChakraProvider>
     </div>
   );
-}
+};
 
 export default App;
