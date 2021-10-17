d24f08e24df956047d760b6943b529720423986b
Livia Emilie Valenti Stokke <livia.e.v.stokke@ntnu.no>
2021-10-18 01:12:15 +0200

    chore: fetch description from pokeapi #20
    * added height and weight from top-level call

--- frontend/src/utils/fetchPokemon.tsx
+++ frontend/src/utils/fetchPokemon.tsx
@@ -1,39 +1,64 @@
-interface Pokemon {
-    id: string,
-    name: string,
-    types: Type[],
+interface APIPokemon {
+  id: string;
+  name: string;
+  height: number;
+  weight: number;
+  types: Type[];
+  sprites: Sprites;
+}
+
+interface Sprites {
+  front_default: string;
+}
+
+interface Type {
+  type: {
+    name: string;
+  };
+}
+
+interface APICharacteristics {
+  descriptions: Description[];
+}
+
+interface Description {
+  description: string;
 }
 
 interface FilteredPokemon {
-    id: string,
-    name: string,
-    types: string[],
-}
-
-interface Type{
-    type: {
-        name: string,
-    }
-}
-
-interface Characteristics {
-    description: string,
+  id: string;
+  name: string;
+  image: string;
+  height: number;
+  weight: number;
+  types: string[];
+  description: string;
 }
 
 export const fetchPokemon = async () => {
-    let pokemonData: FilteredPokemon[] = [];
-    const amountOfPokemon = 5
-    for (let pokemonId = 1; pokemonId <= amountOfPokemon; pokemonId++) {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`).then<Pokemon>((res) => res.json()).then(res => {
-            // fetch(`https://pokeapi.co/api/v2/characteristic/${pokemonId}/`).then<Characteristics>(res => res.json()).then(res => console.log(res))
-            let pokemon = {
-                id: res.id,
-                name: res.name,
-                types: res.types.map(type => type.type.name)
-            }
-            pokemonData.push(pokemon)
-        });
-    }
-    console.log(pokemonData)
-    return pokemonData;
-  };
+  let pokemonData: FilteredPokemon[] = [];
+  const amountOfPokemon = 5;
+  for (let pokemonId = 1; pokemonId <= amountOfPokemon; pokemonId++) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+      .then<APIPokemon>((res) => res.json())
+      .then((res) => {
+        let pokemon: FilteredPokemon = {
+          id: res.id,
+          name: res.name,
+          image: res.sprites.front_default,
+          height: res.height,
+          weight: res.weight,
+          types: res.types.map((type) => type.type.name),
+          description: '',
+        };
+        fetch(`https://pokeapi.co/api/v2/characteristic/${pokemonId}/`)
+          .then<APICharacteristics>((res) => res.json())
+          .then((res) => {
+            // Third element of descriptions is english description
+            pokemon.description = res.descriptions[2].description;
+          });
+        pokemonData.push(pokemon);
+      });
+  }
+  return pokemonData;
+};
