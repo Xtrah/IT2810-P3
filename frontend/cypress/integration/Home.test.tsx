6b64ce1a49207aa1caa58bee984da5f128a6499b
Marcus Haaland <marcuhaa@stud.ntnu.no>
2021-10-19 16:19:49 +0200

    test: add cypress test #19

---
+++ frontend/cypress/integration/Home.test.tsx
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe('Search input and results', () => {
+  let searchText = '';
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+    searchText = 'exegg';
+    cy.get('input[name="searchInput"]').type(searchText);
+  });
+
+  it('search field reacts to user input', () => {
+    cy.get('input[name="searchInput"]').should('have.value', searchText);
+  });
+
+  it('search results show text and image of pokemon', () => {
+    cy.get('p').contains(searchText);
+    cy.get('p')
+      .find('img')
+      .should('have.attr', 'alt')
+      .should('include', searchText);
+  });
+
+  it('search input filters pokemon according to name', () => {
+    /* 
+      Test assumes there are only 2 pokemons with string 'exegg' in name,
+      and only 1 named 'exeggutor'. 
+    */
+    cy.get('p').should('have.length', 2);
+    searchText = 'utor';
+    cy.get('input[name="searchInput"]').type(searchText);
+    cy.get('p').should('have.length', 1);
+  });
+});
