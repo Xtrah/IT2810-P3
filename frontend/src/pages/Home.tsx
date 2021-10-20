60539f74899bcca4f64b032c50a4bb8069bc1f94
HenrikVL <henrik.viken.lied@gmail.com>
2021-10-20 16:43:10 +0200

    feat: add all types in selector #3

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -29,12 +29,6 @@ function Home() {
 
   const { isOpen, onToggle } = useDisclosure();
 
-  /*  const changeSettings = () => {
-    console.log("Click");
-  }; */
-
- 
-
   // Returns UI according to status of data
   const dataResult = () => {
     if (error) {
@@ -74,24 +68,39 @@ function Home() {
       </InputGroup>
 
       <Collapse in={isOpen} animateOpacity>
-          <Box
-            p="40px"
-            mt="4"
-            border="2px"
-            borderColor="red"
-            rounded="md"
-            shadow="md"
+        <Box
+          p="40px"
+          mt="4"
+          border="2px"
+          borderColor="red"
+          rounded="md"
+          shadow="md"
+        >
+          <Select
+            onChange={(e) => setFilterType(e.target.value)}
+            defaultValue={""}
           >
-            <Select onChange={(e) => setFilterType(e.target.value)} defaultValue={""}>
-              <option value="">Show all</option>
-              <option value="normal">Normal</option>
-              <option value="fire">Fire</option>
-              <option value="water">Water</option>
-              <option value="grass">Grass</option>
-              <option value="electric">Electric</option>
-            </Select>
-          </Box>
-        </Collapse>
+            <option value="">Show all</option>
+            <option value="normal">Normal</option>
+            <option value="fire">Fire</option>
+            <option value="water">Water</option>
+            <option value="grass">Grass</option>
+            <option value="ice">Ice</option>
+            <option value="fighting">Fighting</option>
+            <option value="poison">Poison</option>
+            <option value="ground">Ground</option>
+            <option value="flying">Flying</option>
+            <option value="psychic">Psychic</option>
+            <option value="bug">Bug</option>
+            <option value="rock">Rock</option>
+            <option value="ghost">Ghost</option>
+            <option value="dark">Dark</option>
+            <option value="dragon">Dragon</option>
+            <option value="steel">Steel</option>
+            <option value="fairy">Fairy</option>
+          </Select>
+        </Box>
+      </Collapse>
       {dataResult()}
     </Container>
   );
